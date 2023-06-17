3e19e8fe8241cf0844eaca6f6397fd18a499cb8d
Pinya2477 <pinya.su.65@ubu.ac.th>
2023-06-17 12:32:24 +0700

    create first react app

---
+++ src/MyFooter.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function MyFooter() {
+    return (
+        <div className='MyFooter'>
+            
+            <span>Copyright@2023 PINYA SUKWIPHAT</span>
+        </div>
+    );
+}
+
+export default MyFooter;
